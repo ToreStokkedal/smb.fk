0b47b53e5567b05d925820b825d657936d35ead5
Tore Stokkedal <tore.stokkedal@gmail.com>
2022-01-31 20:54:00 +0100

    Twkat flikking

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -16,24 +16,17 @@ function Footer(props) {
                         </ul>
                     </div>
                     <div className="col-7 col-sm-5">
-                        <h5>Our Address</h5>
+                        <h5>Vår adresse</h5>
                         <address>
                             Hotellveien 1<br />
                             Kjønnøya, Bamble<br />
                             Norway<br />
                             <i className="fa fa-phone fa-lg"></i>: +47 999 22 999 <br />
                             <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
-                                hhtell@hotels.net</a>
+                                hotell@hotels.net</a>
                         </address>
                     </div>
-                    <div className="col-12 col-sm-4 align-self-center">
-                        <div className="text-center">
-                            <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
-                            <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
-                            <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                            <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
-                        </div>
-                    </div>
+
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-auto">
